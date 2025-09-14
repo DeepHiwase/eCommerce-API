36e12317d8fd11ce593250012ef80672cd83a058
Deep <diphiwase0987@gmail.com>
2025-09-15 01:18:00 +0530

    add user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,50 @@
+/**
+ * @copyright 2025 deephiwase
+ * @license Apache-2.0
+ */
+
+// Node Modules
+import { Schema, model, type Document } from "mongoose";
+
+export interface UserDocument extends Document {
+	email: string;
+	password: string;
+	role: "retailer" | "customer";
+	verified: boolean;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+const userSchema = new Schema<UserDocument>(
+	{
+		email: {
+			type: String,
+			unique: true,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		role: {
+			type: String,
+			enum: {
+				values: ["retailer", "customer"],
+				message: "{VALUE} is not supported",
+			},
+			default: "customer",
+		},
+		verified: {
+			type: Boolean,
+			required: true,
+			default: false,
+		},
+	},
+	{
+		timestamps: true,
+	},
+);
+
+const UserModel = model<UserDocument>("User", userSchema);
+
+export default UserModel;
