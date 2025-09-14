20e8c957c8ab7521eee9ded81472155760a6b715
Deep <diphiwase0987@gmail.com>
2025-09-15 00:40:12 +0530

    Handle zod error in error handling middleware

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -3,15 +3,33 @@
  * @license Apache-2.0
  */
 
+// Node Modules
+import z from "zod";
 // Custom Modules
 import { logger } from "@/lib/winston";
 // Types
-import type { ErrorRequestHandler } from "express";
+import type { ErrorRequestHandler, Response } from "express";
 // Constants
-import { INTERNAL_SERVER_ERROR } from "@/constants/http";
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from "@/constants/http";
+
+const handleZodError = (res: Response, error: z.ZodError) => {
+	const errors = error.issues.map((err) => ({
+		path: err.path.join("."),
+		message: err.message,
+	}));
+
+	return res.status(BAD_REQUEST).json({
+		message: error.message,
+		errors,
+	});
+};
 
 const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
 	logger.error(`PATH: ${req.path}`, error);
+
+	if (error instanceof z.ZodError) {
+		return handleZodError(res, error);
+	}
 
 	return res.status(INTERNAL_SERVER_ERROR).send("Internal Server Error");
 };
